4f26ebb29daf05f3abcb11dd1f85e66468596cb1
David Zoller <david.l.zoller@gmail.com>
2015-08-23 16:03:33 -0500

    PML files
    Practical machine learning course project

---
+++ pml_proj.R
@@ -0,0 +1,25 @@
+---
+title: "PML Project"
+author: "datadavidz"
+date: "Thursday, August 20, 2015"
+output: html_document
+---
+
+##Executive Summary
+Study participants were asked to perform barbell lifts using the correct procedure and incorrectly in 5 different ways.  Data from accelerometers on the belt, forearm, arm and barbell of 6 participants was used to build a model based on a random forest algorithm to accurately classify new lifts.
+
+This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+
+When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
+
+```{r}
+summary(cars)
+```
+
+You can also embed plots, for example:
+
+```{r, echo=FALSE}
+plot(cars)
+```
+
+Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
